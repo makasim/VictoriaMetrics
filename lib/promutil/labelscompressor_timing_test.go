a5989020997e1114b4ab1b34403d4d4b472abf4a
Max Kotliar <mkotlyar@victoriametrics.com>
2025-08-22 10:45:03 +0300

    add 1m bench

--- lib/promutil/labelscompressor_timing_test.go
+++ lib/promutil/labelscompressor_timing_test.go
@@ -52,10 +52,10 @@ func BenchmarkLabelsCompressorDecompress(b *testing.B) {
 	})
 }
 
-func BenchmarkLabelsCompressor10M(b *testing.B) {
+func BenchmarkLabelsCompressor_Preload1M_Compress(b *testing.B) {
 	var lc LabelsCompressor
 
-	for i := 0; i < 100_000; i++ {
+	for i := 0; i < 1_000_000; i++ {
 		labels := []prompb.Label{
 			{
 				Name:  "instance",
@@ -82,6 +82,46 @@ func BenchmarkLabelsCompressor10M(b *testing.B) {
 				dst = lc.Compress(dst, labels)
 			}
 			Sink.Add(uint64(len(dst)))
+		}
+	})
+}
+
+func BenchmarkLabelsCompressor_Preload1M_Decompress(b *testing.B) {
+	var lc LabelsCompressor
+
+	for i := 0; i < 1_000_000; i++ {
+		labels := []prompb.Label{
+			{
+				Name:  "instance",
+				Value: fmt.Sprintf("1.2.3.%d", i),
+			},
+			{
+				Name:  "job",
+				Value: fmt.Sprintf("pod%d", i),
+			},
+		}
+		lc.Compress(nil, labels)
+	}
+
+	series := newTestSeries(100, 10)
+	datas := make([][]byte, len(series))
+	var dst []byte
+	for i, labels := range series {
+		dstLen := len(dst)
+		dst = lc.Compress(dst, labels)
+		datas[i] = dst[dstLen:]
+	}
+
+	b.ReportAllocs()
+	b.SetBytes(int64(len(series)))
+
+	b.RunParallel(func(pb *testing.PB) {
+		var labels []prompb.Label
+		for pb.Next() {
+			for _, data := range datas {
+				labels = lc.Decompress(labels[:0], data)
+			}
+			Sink.Add(uint64(len(labels)))
 		}
 	})
 }
